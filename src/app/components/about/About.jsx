6761fa84452da3bcc26a6b166f36567cc8d01ab1
gschrock <gabrielschrock@gmail.com>
2017-07-23 11:54:18 -0700

    first

---
+++ src/app/components/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react'
+
+function About() {
+  return (
+    <div className="container about">
+      <h1>About</h1>
+      <Button>Click</Button>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/app/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import App from './components/App';
+import Home from './components/home/Home';
+import About from './components/about/About';
+
+import reducers from './reducers';
+
+import './components/bundle.scss';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />;
+        <Route path="/about" component={About} />
+      </Route>
+    </Router>
+  </Provider>
+  , document.getElementById('react-root'));
