1227f824cced10becf82d412f8e1569bece423d2
gschrock <gabrielschrock@gmail.com>
2017-12-10 16:25:16 -0800

    (feat) Adds supplemental tables

---
+++ src/app/utils/supplemental/attitude.js
@@ -0,0 +1,18 @@
+import Roll from 'roll';
+
+export function attitude() {
+  const roll = new Roll();
+  const attitudeRoll = roll.roll('3d4');
+
+  if (attitudeRoll.result === 3 || attitudeRoll.result === 4) {
+    return '34';
+  }
+  
+  if (attitudeRoll.result >= 5 && attitudeRoll.result <= 10) {
+    return '510';
+  }
+  
+  if (attitudeRoll.result === 11 || attitudeRoll.result === 12) {
+    return '1112';
+  }
+};
