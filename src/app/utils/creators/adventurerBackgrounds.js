e002115a5e5ec3746514a70b3532a4b65dfc716f
Kristine Yabut <amenotu@users.noreply.github.com>
2017-12-17 17:00:20 -0800

    (feat) Add handling for life events edge cases (#11)
    * Add roll dependency to package
    
    * (feat) Add file architecture for utils and setup initialState
    
    * (feat) Separate files into categories
    
    * (feat) Add birthOrder thunks, actions and reducer
    
    * (feat) Add event edge case handling to Life Events thunks
    
    * (style) Code cleanup for edge cases in Life Events

---
+++ src/app/utils/creators/adventurerBackgrounds.js
@@ -0,0 +1,22 @@
+const backgrounds = {
+  1: 'Acolyte',
+  2: 'Charlatan',
+  3: 'Criminal',
+  4: 'Entertainer',
+  5: 'Folk Hero',
+  6: 'Guild Artisan',
+  7: 'Hermit',
+  8: 'Noble',
+  9: 'Outlander',
+  10: 'Sage',
+  11: 'Sailor',
+  12: 'Soldier',
+  13: 'Urchin',
+};
+
+export function adventurerBackgrounds() {
+  const adventurerBackgroundsRoll = Math.floor(Math.random() * 13) + 1;
+  const title = backgrounds[adventurerBackgroundsRoll];
+
+  return title;
+};
---
+++ src/app/utils/creators/adventurerCreator.js
@@ -0,0 +1,20 @@
+import Roll from 'roll';
+import { adventurerBackgrounds } from './adventurerBackgrounds.js';
+import { race } from '../supplemental/race.js';
+import { supplementalClass } from '../supplemental/supplementalClass.js';
+import { alignment } from '../supplemental/alignment.js';
+import { status } from '../supplemental/status.js';
+import { attitude } from '../supplemental/attitude.js';
+
+export function createAdventurer() {
+  const adventurer = {
+    race: race(),
+    class: supplementalClass(),
+    background: adventurerBackgrounds(),
+    alignment: alignment(),
+    status: status(),
+    attitude: attitude(),
+  };
+
+  return adventurer;
+};
